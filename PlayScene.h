420c789cb86c046195cd6ac1dae5da918adeb0b6
Seongyeol Yun <02tjdduf@gmail.com>
2022-11-27 02:13:44 +0900

    Add PlayScene

---
+++ PlayScene.h
@@ -0,0 +1,20 @@
+#pragma once
+#include "CarManager.h"
+#include "SceneManager.h"
+
+class PlayScene : public Scene {
+public:
+	virtual void initialize() override;
+
+	virtual void inputKeyboard(unsigned char, int, int) override;
+
+	virtual void inputSpecialKeyboard(int, int, int) override;
+
+	virtual void inputMouse(int, int, int, int) override;
+
+	virtual void update() override;
+
+	virtual void render() override;
+
+	virtual void release() override;
+};
