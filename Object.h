89cdee5862532b357368d3e7bf15b15dcc89e7bf
Seongyeol Yun <02tjdduf@gmail.com>
2022-11-27 02:15:09 +0900

    Add Object interface class

---
+++ Object.h
@@ -0,0 +1,33 @@
+#pragma once
+#include "pch.h"
+
+class Object {
+protected:
+	std::vector<glm::vec3> verticies;
+	std::vector<glm::vec3> normals;
+	std::vector<glm::vec3> colors;
+	std::vector<glm::vec2> uvs;
+	std::vector<GLuint> vertexIndices;
+	std::vector<GLuint> normalIndices;
+	std::vector<GLuint> uvIndices;
+
+	GLuint vao;
+	GLuint vertexVbo;
+	GLuint normalVbo;
+	GLuint colorVbo;
+	GLuint uvVbo;
+	
+	bool isObjInit{ false };
+public:
+	Object();
+
+	virtual ~Object();
+
+	virtual void initialize() = 0;
+	virtual void readObj(const char*) = 0;
+	virtual void update() = 0;
+	virtual void render() = 0;
+	virtual void release() = 0;
+
+	friend class Shader;
+};
