f5d312ad3f91ea0580a6f0207b908dd4ef5f3a42
Seongyeol Yun <02tjdduf@gmail.com>
2022-11-27 02:17:00 +0900

    Add Scene Interaface

---
+++ Scene.h
@@ -0,0 +1,24 @@
+#pragma once
+#include "SceneManager.h"
+#include "pch.h"
+
+class Scene {
+public:
+	Scene() = default;
+
+	virtual ~Scene() = default;
+
+	virtual void initialize() = 0;
+
+	virtual void inputKeyboard(unsigned char key, int x, int y) = 0;
+	
+	virtual void inputSpecialKeyboard(int key, int x, int y) = 0;
+
+	virtual void inputMouse(int button, int state, int x, int y) = 0;
+
+	virtual void update() = 0;
+
+	virtual void render() = 0;
+
+	virtual void release() = 0;
+};
