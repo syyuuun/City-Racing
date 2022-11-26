940dfd99031094ceb409dda994120673d22fc1e4
Seongyeol Yun <02tjdduf@gmail.com>
2022-11-27 02:13:03 +0900

    Add GameFrameWork

---
+++ GameFrameWork.cpp
@@ -0,0 +1,73 @@
+#include "GameFrameWork.h"
+
+GameFrameWork* GameFrameWork::pInst = nullptr;
+
+GameFrameWork::GameFrameWork()
+{
+
+}
+
+GameFrameWork* GameFrameWork::getInstance()
+{
+	if (nullptr == pInst)
+		pInst = new GameFrameWork;
+
+	return pInst;
+}
+
+GLvoid GameFrameWork::initialize()
+{
+	// 모든 객체 초기화는 여기서 진행
+	Shader::getInstance()->glGenerate();
+	Shader::getInstance()->initBuffer();
+	Shader::getInstance()->initShader();
+}
+
+GLvoid GameFrameWork::renderScene()
+{
+	glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glEnable(GL_DEPTH_TEST);
+
+	Shader::getInstance()->initBuffer();
+	Camera::getInstance()->update();
+	SceneManager::getInstance()->render();
+
+	glutSwapBuffers();
+}
+
+GLvoid GameFrameWork::reshape(int w, int h)
+{
+	glViewport(0, 0, w, h);
+}
+
+GLvoid GameFrameWork::inputKeyboard(unsigned char key, int x, int y)
+{
+	SceneManager::getInstance()->inputKeyboard(key, x, y);
+	glutPostRedisplay();
+}
+
+GLvoid GameFrameWork::inputSpecialKeyboard(int key, int x, int y)
+{
+	SceneManager::getInstance()->inputSpecialKeyboard(key, x, y);
+	glutPostRedisplay();
+}
+
+GLvoid GameFrameWork::inputMouse(int button, int state, int x, int y)
+{
+	SceneManager::getInstance()->inputMouse(button, state, x, y);
+	glutPostRedisplay();
+}
+
+GLvoid GameFrameWork::update()
+{
+	SceneManager::getInstance()->update();
+}
+
+GLvoid GameFrameWork::release()
+{
+	if (nullptr == pInst) {
+		delete pInst;
+		pInst = nullptr;
+	}
+}
--- GameFrameWork.h
+++ GameFrameWork.h
@@ -1,68 +1,32 @@
 #pragma once
+#include "pch.h"
+#include "Shader.h"
+#include "Light.h"
+#include "Camera.h"
+#include "SceneManager.h"
+#include "CarManager.h"
 
 class GameFrameWork {
+private:
+	static GameFrameWork* pInst;
 public:
-	static GameFrameWork* getInstance()
-	{
-		static GameFrameWork* pInst = nullptr;
-
-		if (nullptr == pInst)
-			pInst = new GameFrameWork;
-
-		return pInst;
-	}
+	GameFrameWork();
 public:
-	GLvoid renderScene()
-	{
-		glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
-		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-		glEnable(GL_DEPTH_TEST);
-
-
-
-
-
-
-
-
-
-
-		glutSwapBuffers();
-	}
-
-	GLvoid reshape(int w, int h)
-	{
-		glViewport(0, 0, w, h);
-	}
-
-	GLvoid inputKeyboard(unsigned char key, int x, int y)
-	{
-		switch (key)
-		{
-		default:
-			break;
-		}
-
-		glutPostRedisplay();
-	}
-
-	GLvoid inputSpecialKeyboard(int key, int x, int y)
-	{
-		switch (key)
-		{
-		default:
-			break;
-		}
-		glutPostRedisplay();
-	}
-
-	GLvoid inputMouse()
-	{
-		glutPostRedisplay();
-	}
+	static GameFrameWork* getInstance();
 	
-	GLvoid update()
-	{
+	GLvoid initialize();
 
-	}
+	GLvoid renderScene();
+
+	GLvoid reshape(int w, int h);
+
+	GLvoid inputKeyboard(unsigned char key, int x, int y);
+
+	GLvoid inputSpecialKeyboard(int key, int x, int y);
+
+	GLvoid inputMouse(int,int,int,int);
+	
+	GLvoid update();
+
+	GLvoid release();
 };
