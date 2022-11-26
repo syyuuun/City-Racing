8402b95fdfee47840303cc2b81f1cfd754dd3864
Seongyeol Yun <02tjdduf@gmail.com>
2022-11-27 02:11:42 +0900

    Add Bmw class

---
+++ Bmw.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include "CarManager.h"
+
+class Bmw : public Car {
+public:
+	Bmw() = default;
+
+	virtual void initialize() override;
+
+	virtual void readObj(const char* objName) override;
+
+	virtual void inputKeyboard(unsigned char key, int x, int y) override;
+
+	virtual void inputSpecialKeyboard(int key, int x, int y) override;
+
+	virtual void inputMouse(int button, int state, int x, int y) override;
+
+	virtual void update() override;
+
+	virtual void render() override;
+
+	virtual void release() override;
+
+	friend class Shader;
+};
