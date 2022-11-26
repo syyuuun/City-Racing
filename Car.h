da49d05a2f50d51cd9982401a76c00fd2314262e
Seongyeol Yun <02tjdduf@gmail.com>
2022-11-27 02:17:23 +0900

    Add Car.h

---
+++ Car.h
@@ -0,0 +1,20 @@
+#include "pch.h"
+#include "Shader.h"
+#include "Object.h"
+#include "CarManager.h"
+
+class Car : public Object {
+protected:
+	glm::vec3 positionVector;
+	glm::vec3 scaleVector;
+	GLfloat rotationDegree{ 0.0f };
+
+public:
+	virtual void inputKeyboard(unsigned char, int, int) = 0;
+
+	virtual void inputSpecialKeyboard(int, int, int) = 0;
+
+	virtual void inputMouse(int, int, int, int) = 0;
+
+	friend class Shader;
+};
