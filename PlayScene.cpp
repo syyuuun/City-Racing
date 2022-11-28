5949ab8f20a5cb718c260896496d300bd6e4403b
Seongyeol Yun <02tjdduf@gmail.com>
2022-11-28 20:34:09 +0900

    Update PlayScene.cpp

--- PlayScene.cpp
+++ PlayScene.cpp
@@ -3,6 +3,16 @@
 void PlayScene::initialize()
 {
 	std::cout << "Enter Play Scene" << "\n";	
+
+	// Road Load
+	 
+	// Obstacle Road
+
+	// Car Load
+	
+	// Camera Load
+	 
+	// Light Load
 }
 
 void PlayScene::inputKeyboard(unsigned char key, int x, int y)
@@ -10,7 +20,7 @@ void PlayScene::inputKeyboard(unsigned char key, int x, int y)
 	switch (key)
 	{
 	case VK_ESCAPE:
-		SceneManager::getInstance()->changeScene(SceneType::END);
+		SceneManager::getInstance()->changeScene(SceneManager::SceneType::END);
 		break;
 	default:
 		break;
@@ -19,7 +29,15 @@ void PlayScene::inputKeyboard(unsigned char key, int x, int y)
 
 void PlayScene::inputSpecialKeyboard(int key, int x, int y)
 {
-
+	switch (key)
+	{
+	case GLUT_KEY_LEFT:
+		break;
+	case GLUT_KEY_RIGHT:
+		break;
+	default:
+		break;
+	}
 }
 
 void PlayScene::inputMouse(int button, int state, int x, int y)
@@ -29,12 +47,28 @@ void PlayScene::inputMouse(int button, int state, int x, int y)
 
 void PlayScene::update()
 {
+	// Road Update
+
+	// Obstacle update
+
+	// Car update
+	Camera::getInstance()->update();
+
+	// Light update
+	Light::getInstance()->update();
 }
 
 void PlayScene::render()
 {
+	// Road render
+
+	// Obstacle render
+
+	// Car render
+	CarManager::getInstance()->render();
 }
 
 void PlayScene::release()
 {
+
 }
