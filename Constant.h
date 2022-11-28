0ed2b8702e63354cf65b876eebe78ada130656f7
Seongyeol Yun <02tjdduf@gmail.com>
2022-11-28 20:33:58 +0900

    Update Constant.h

--- Constant.h
+++ Constant.h
@@ -2,7 +2,7 @@
 #include "pch.h"
 
 namespace Constant {
-	constexpr unsigned int winWidth{ 800 };
-	constexpr unsigned int  winHeight{ 600 };
+	constexpr unsigned int winWidth{ 600 };
+	constexpr unsigned int  winHeight{ 700 };
 	constexpr char projectName[] = { "City Racing" };
 }
